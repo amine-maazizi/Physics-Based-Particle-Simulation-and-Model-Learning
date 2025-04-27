56e0a2e0a1fd7fc8396201072dade116911df7a4
amine-maazizi <Aminema1000@gmail.com>
2025-04-27 16:46:54 +0200

    Experimentend with an RNN and a PINN

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from simulation.simulation import Simulation
 
 if __name__ == "__main__":
     simulation = Simulation(
-        mode='testing',
-        particle_number=1,         # Single particle for simplicity, as per your neural network design
+        mode='testing',         # Set to 'training' for training mode
+        particle_number=10,         # Single particle for simplicity, as per your neural network design
         nb_trials=100,            # Sufficient trials for robust dataset
         trial_duration=5.0,              # 5 seconds per trial
         trial_stop_velocity=0.1          # Stop when velocity < 0.1 m/s
